639cce474bb7523f9af686031670cff493f9ce7d
Ivan Bochenkov <arcticmonkey777@yandex.ru>
2023-11-13 21:55:18 +0200

    Add tests

---
+++ AreaCalculator/ShapeAreaCalculatorTests/AreaCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeAreaCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaCalculator.Tests
+{
+    [TestClass()]
+    public class AreaCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateAreaTest()
+        {
+
+            AreaCalculator areaCalculator = new AreaCalculator();
+            double circleArea = areaCalculator.CalculateArea<Circle>(5);
+            double triangleArea = areaCalculator.CalculateArea<Triangle>(3, 20, 5);
+            
+        }
+    }
+}
